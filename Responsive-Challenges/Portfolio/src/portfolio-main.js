9e435604bf53745f1e9014e5cbe6e49cd276e6a8
donutenthusiast <95315673+sylviaintech@users.noreply.github.com>
2022-05-31 21:52:10 +0200

    Introduction of media query for narrow devices. Styling changes to the navbar menu across device width changes and addition of images for a carousel.
    Updates to HTML, CSS and JS as well as the folder structure.

--- Responsive-Challenges/Portfolio/src/portfolio-main.js
+++ Responsive-Challenges/Portfolio/src/portfolio-main.js
@@ -1,7 +1,10 @@
 function openNavLarge() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("mySidenav").style.height = "100%";
-  document.getElementById("mySidenav").style.padding = "50px";
+  document.getElementById("mySidenav").style.paddingLeft = "50px";
+  document.getElementById("mySidenav").style.paddingRight = "50px";
+  document.getElementById("mySidenav").style.paddingBottom = "50px";
+  document.getElementById("mySidenav").style.paddingTop = "50px";
   document.getElementById("main").style.marginLeft = "250px";
 }
 
@@ -9,19 +12,28 @@ function closeNavLarge() {
   document.getElementById("main").style.marginLeft = "0";
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("mySidenav").style.height = "0";
-  document.getElementById("mySidenav").style.padding = "0";
+  document.getElementById("mySidenav").style.paddingLeft = "0";
+  document.getElementById("mySidenav").style.paddingRight = "0";
+  document.getElementById("mySidenav").style.paddingBottom = "0";
+  document.getElementById("mySidenav").style.paddingTop = "0";
 }
 
 function openNavSmall() {
   document.getElementById("mySidenav").style.width = "100%";
   document.getElementById("mySidenav").style.height = "70px";
-  document.getElementById("mySidenav").style.padding = "10px 5px";
-  document.getElementById("main").style.marginLeft = "inherit";
+  document.getElementById("mySidenav").style.paddingLeft = "0";
+  document.getElementById("mySidenav").style.paddingRight = "0";
+  document.getElementById("mySidenav").style.paddingBottom = "0";
+  document.getElementById("mySidenav").style.paddingTop = "15px";
+  document.getElementById("main").style.marginLeft = "0";
 }
 
 function closeNavSmall() {
   document.getElementById("mySidenav").style.height = "0";
-  document.getElementById("mySidenav").style.padding = "0";
+  document.getElementById("mySidenav").style.paddingLeft = "0";
+  document.getElementById("mySidenav").style.paddingRight = "0";
+  document.getElementById("mySidenav").style.paddingBottom = "0";
+  document.getElementById("mySidenav").style.paddingTop = "0";
 }
 
 function startCloseMenu() {
