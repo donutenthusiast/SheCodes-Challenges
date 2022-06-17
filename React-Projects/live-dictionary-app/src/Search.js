64e2a585e82f1627c2d4d76cae9f0525d8e93555
donutenthusiast <95315673+sylviaintech@users.noreply.github.com>
2022-06-17 14:40:38 +0200

    Installed axios and integrated free dictionary api with the search word and isLoaded useState
    https://dictionaryapi.dev/

--- React-Projects/live-dictionary-app/src/Search.js
+++ React-Projects/live-dictionary-app/src/Search.js
@@ -1,25 +1,47 @@
 import React, {useState} from "react";
 
+import axios from "axios";
+
 import "./Search.css";
 
 export default function Search() {
+  const [isloaded, setIsLoaded] = useState(false);
   const [word, setWord] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching for your word: ${word}`);
+    const apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    axios.get(apiURL).then(displayResults);
   }
 
   function searchTerm(event) {
     setWord(event.target.value);
   }
 
-  return (
-    <div className="text-center pb-3 search-form">
-      <form>
-        <input type="text" placeholder="Enter a word" onChange={searchTerm} />{" "}
-        <button onClick={handleSubmit}>Submit</button>
-      </form>
-    </div>
-  );
+  function displayResults(response) {
+    console.log(response.data);
+    setIsLoaded(true);
+  }
+
+  if (isloaded) {
+    return (
+      <div className="text-center pb-3 search-form">
+        <form>
+          <input type="text" placeholder="Enter a word" onChange={searchTerm} />{" "}
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
+        <p>The word you are looking for is {word}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="text-center pb-3 search-form">
+        <form>
+          <input type="text" placeholder="Enter a word" onChange={searchTerm} />{" "}
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
